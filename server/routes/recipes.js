6f931f7c1acbf405abc38e9ac9aab80b81d730cb
Your Name <you@example.com>
2017-09-28 10:56:41 +0100

    add more contents

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -2,6 +2,6 @@ import express from 'express';
 import recipeController from '../controller/recipes';
 
 let router = express.Router();
-router.get('/', recipeController.test)
+router.get('/', recipeController.test) 
 
 export default router;
