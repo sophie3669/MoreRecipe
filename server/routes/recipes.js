b38fe13e040c6231474ec93a27b9564107534f35
Your Name <you@example.com>
2017-10-05 08:16:45 +0100

    "ft_Authenticated_user_modify"

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -7,9 +7,9 @@ const recipes = MoreRecipes.recipesController;
 
 const router = express.Router();
 router.post('/api/recipes', recipes.add);
-router.put('/api/recipes/:recipeId', recipes.update);
-router.delete('/api/recipes/:recipeId', recipes.delete);
-router.get('/api/recipes', recipes.getRecipe);
+//router.put('/api/recipes/:recipeId', recipes.update);
+//router.delete('/api/recipes/:recipeId', recipes.delete);
+//router.get('/api/recipes', recipes.getRecipe);
 // router.post('/api/recipes/:recipeId/reviews', recipes.reviewRecipe);
 // router.post('/api/recipes/:recipeId/vote', recipes.vote);
 
